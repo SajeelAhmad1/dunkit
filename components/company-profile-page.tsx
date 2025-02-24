43c21f8dc915785ded6fe68349e5337979c6188a
nauroz <alinauroze@hotmail.com>
2025-02-24 15:45:06 +0500

    fix: map

--- components/company-profile-page.tsx
+++ components/company-profile-page.tsx
@@ -113,11 +113,14 @@ const CompanyProfilePage = () => {
       <div className="mt-[80px] lg:mt-[200px]  flex-col justify-center">
         <div>
           <h1 className="text-2xl lg:text-5xl flex justify-center ">
-            <FormattedMessage id={"CompanyProfilePage.Tokyo"} defaultMessage={"Tokyo Head Office"}/>
+            <FormattedMessage 
+              id={"CompanyProfilePage.Tokyo"} 
+              defaultMessage={"Tokyo Head Office"}
+            />
           </h1>
         </div>
         <div className="flex justify-center  mx-auto  m-3 lg:max-w-[800px] lg:h-[450] ">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d425289.990533232!2d72.75643905071773!3d33.61625093699602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dfbfd07891722f%3A0x6059515c3bdb02b6!2sIslamabad%2C%20Pakistan!5e0!3m2!1sen!2s!4v1740153779582!5m2!1sen!2s" 
+        <iframe src="https://www.google.com/maps?q=35.655384202264415,139.7548247712674&z=15&output=embed" 
         // {style="border:0;"} 
         // {allowfullscreen="" }
         loading="lazy" 
